894dedbac715a812b727749ae3de676af15fab9f
Chris Zuber <shgysk8zer0@gmail.com>
2015-01-18 23:12:39 -0800

    New Trait: Path Info
    For getting info such as:
    * Directory name
    * File name
    * Base name
    * Extension

---
+++ traits/path_info.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @author Chris Zuber <shgysk8zer0@gmail.com>
+ * @package shgysk8zer0\Core_API
+ * @version 1.0.0
+ * @copyright 2015, Chris Zuber
+ * @license http://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace shgysk8zer0\Core_API\Traits;
+
+trait Path_Info
+{
+	protected $dirname, $basename, $extension, $filename;
+	final protected function getInfo($path)
+	{
+		list(
+			$this->dirname,
+			$this->basename,
+			$this->extension,
+			$this->filename
+		) = array_values(pathinfo($path));
+	}
+}
