aa3b5df0bfcd826351936ed243c626ffbf7889c4
Chris Zuber <shgysk8zer0@gmail.com>
2015-01-20 05:00:16 -0800

    Added socket client and server traits
    Incomplete... Needs testing and more documentation, but includes at
    least the core functionality.

---
+++ traits/socket_client.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * @author Chris Zuber <shgysk8zer0@gmail.com>
+ * @package shgysk8zer0\Core_API
+ * @version 1.0.0
+ * @copyright 2015, Chris Zuber
+ * @license http://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace shgysk8zer0\Core_API\Traits;
+
+/**
+ * This trait converts Socket functions into OOP.
+ * It uses socket* (camelCase) method names instead of socket_* functions.
+ */
+trait Socket_Client
+{
+	protected $socket_client;
+
+	/**
+	 * [socketAccept description]
+	 *
+	 * @param [type] $socket [description]
+	 * @return self
+	 */
+	final public function socketAccept($socket = null)
+	{
+		if (gettype($socket) === 'resource') {
+			$this->socket_client = socket_accept($socket);
+		} else {
+			throw new InvalidArgumentException(__METHOD__ . ' expects $socket to be an instance of resource. ' . gettype($resource) . ' given instead');
+		}
+		return $this;
+	}
+
+	/**
+	 * [socketWrite description]
+	 *
+	 * @param string $message [description]
+	 * @param int    $length  [description]
+	 * @return self
+	 */
+	final public function socketWrite($message = '', $length = null)
+	{
+		if (!is_int($length)) {
+			$length = strlen($message);
+		}
+		socket_write($this->socket_client, $message, $length);
+		return $this;
+	}
+}
