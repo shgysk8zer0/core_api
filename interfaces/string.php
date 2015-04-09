2c984f25203ac135ce850224491daab126b8c79a
Chris Zuber <shgysk8zer0@gmail.com>
2015-04-08 19:53:56 -0700

    New interface String
    Useful for typehinting to verify that a class can be converted to a
    string.

---
+++ interfaces/string.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @author Chris Zuber <shgysk8zer0@gmail.com>
+ * @package shgysk8zer0\Core_API
+ * @version 1.0.0
+ * @copyright 2015, Chris Zuber
+ * @license http://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace shgysk8zer0\Core_API\Interfaces;
+
+/**
+ * Not a useful interface, but it does allow typehinting to ensure compatibility
+ */
+interface String
+{
+	/**
+	 * Method called whenever class object is used as a string
+	 *
+	 * @return string Up to class implementing to determine how to build string
+	 */
+	public function __toString();
+}
