99db1812eec99b5eb00aafc661aa71eec82cba3e
Chris Zuber <shgysk8zer0@gmail.com>
2015-04-28 08:05:16 -0700

    Updated Regular Expression class
    * Corrected "@package"
    * Added "HTML_COMMENT" (does not match conditional comments)

--- abstracts/regexp.php
+++ abstracts/regexp.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @author Chris Zuber
- * @package shgysk8zer0\Core
+ * @package shgysk8zer0\Core_API
  * @version 1.0.0
  * @since 1.0.0
  * @copyright 2015, Chris Zuber
@@ -27,17 +27,18 @@ namespace shgysk8zer0\Core_API\Abstracts;
  */
 abstract class RegExp
 {
-	const TEXT     = "(\w+ ?)+";
-	const NAME     = "[A-Za-z]{3,30}";
-	const PASSWORD = "(?=^.{8,35}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*";
-	const EMAIL    = ".+@.+\.+[\w]+";
-	const URL      = "(https?:\/\/)?[\S]+\.[\S]+";
-	const TEL      = "([+]?[1-9][-]?)?((\([\d]{3}\))|(\d{3}[-]?))\d{3}[-]?\d{4}";
-	const NUMBER   = "\d+(\.\d+)?";
-	const COLOR    = "#?([a-fA-F0-9]{6}|[a-fA-F0-9]{3})";
-	const DATE     = "((((0?)[1-9])|(1[0-2]))(-|/)(((0?)[1-9])|([1-2][\d])|3[0-1])(-|/)\d{4})|(\d{4}-(((0?)[1-9])|(1[0-2]))-(((0?)[1-9])|([1-2][\d])|3[0-1]))";
-	const TIME     = "(([0-1]?\d)|(2[0-3])):[0-5]\d";
-	const DATETIME = '(19|20)\d{2}-(0?[1-9]|1[0-2])-(0?[1-9]|[12]\d?|3[01])T([01]\d|2[0-3])(:[0-5]\d)+';
-	const WEEK     = '\d{4}-W\d{2}';
-	const CREDIT   = "\d{13,16}";
+	const TEXT         = "(\w+ ?)+";
+	const NAME         = "[A-Za-z]{3,30}";
+	const PASSWORD     = "(?=^.{8,35}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*";
+	const EMAIL        = ".+@.+\.+[\w]+";
+	const URL          = "(https?:\/\/)?[\S]+\.[\S]+";
+	const TEL          = "([+]?[1-9][-]?)?((\([\d]{3}\))|(\d{3}[-]?))\d{3}[-]?\d{4}";
+	const NUMBER       = "\d+(\.\d+)?";
+	const COLOR        = "#?([a-fA-F0-9]{6}|[a-fA-F0-9]{3})";
+	const DATE         = "((((0?)[1-9])|(1[0-2]))(-|/)(((0?)[1-9])|([1-2][\d])|3[0-1])(-|/)\d{4})|(\d{4}-(((0?)[1-9])|(1[0-2]))-(((0?)[1-9])|([1-2][\d])|3[0-1]))";
+	const TIME         = "(([0-1]?\d)|(2[0-3])):[0-5]\d";
+	const DATETIME     = '(19|20)\d{2}-(0?[1-9]|1[0-2])-(0?[1-9]|[12]\d?|3[01])T([01]\d|2[0-3])(:[0-5]\d)+';
+	const WEEK         = '\d{4}-W\d{2}';
+	const CREDIT       = "\d{13,16}";
+	const HTML_COMMENT = '<!--(?!\s*(?:\[if [^\]]+]|<!|>))(?:(?!-->).)*-->';
 }
