7f46d14382f9e30bd3f4da16bcc0c7d4ece4b6e3
Chris Zuber <shgysk8zer0@gmail.com>
2015-04-29 08:26:44 -0700

    Traits adding magic methods to DOMDocument & DOMElement
    Provides `__set`, `__get`, `__isset`, & `__unset`
    `DOMElement` and `DOMDocument` have an abstract/required `__toString`
    `DOMElement` also has `__invoke` method, which imports HTML/XML

---
+++ traits/magic/domdocument.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * @author Chris Zuber <shgysk8zer0@gmail.com>
+ * @package shgysk8zer0\Core_API
+ * @version 1.0.0
+ * @copyright 2015, Chris Zuber
+ * @license http://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace shgysk8zer0\Core_API\Traits\Magic;
+/**
+ * Provides magic methods to \DOMDocument... Make sure to extend it
+ * @see https://php.net/manual/en/class.domdocument.php
+ */
+trait DOMDocument
+{
+	/**
+	 * Magic alias of appendChild + createElement
+	 *
+	 * @param string $name  The tag name of the element.
+	 * @param string $value The value of the element.
+	 * @see https://php.net/manual/en/domdocument.createelement.php
+	 */
+	final public function __set($name, $value)
+	{
+		if (property_exists($this, 'body')) {
+			$node = $this->body->appendChild($this->createElement($name));
+			if (is_string($value)) {
+				$node->nodeValue = $value;
+			} elseif (is_object($value) and $value instanceof \DOMNode) {
+				$node->appendChild($value);
+			} elseif (isset($value)) {
+				trigger_error(
+					sprintf(
+						'%s expects paramater 2 to be either a string or node, %s given',
+						__METHOD__,
+						gettype($value)
+					),
+					E_USER_WARNING
+				);
+			}
+		} else {
+			trigger_error('Attempting to append when there is no root element', E_USER_WARNING);
+		}
+
+	}
+
+	/**
+	 * Magic alias of getElementsByTagName
+	 *
+	 * @param  string $name  The local name (without namespace) of the tag
+	 * @return \DOMNodeList  A new DOMNodeList object containing all the matched elements.
+	 * @see https://php.net/manual/en/domdocument.getelementsbytagname.php
+	 */
+	final public function __get($name)
+	{
+		return $this->getElementsByTagName($name);
+	}
+
+	/**
+	 * Checks if any matches found by checking length of NodeList
+	 *
+	 * @param  string  $name Tag name to search for
+	 * @return bool          Whether or not any matching children are found
+	 */
+	final public function __isset($name)
+	{
+		return $this->getElementsByTagName($name)->length > 0;
+	}
+
+	/**
+	 * Remove all child nodes with Tag Name $name
+	 *
+	 * @param string $name Query for __get to return NodeList of
+	 * @return void
+	 */
+	final public function __unset($name)
+	{
+		$nodes = $this->__get($name);
+		$length = $nodes->length;
+		for ($i=0; $i < $length; $i++) {
+			$this->removeChild($nodes->item($i));
+		}
+	}
+
+	/**
+	 * Abstract method for when class is used as a string
+	 *
+	 * @param void
+	 * @return string XML, HTML... whatever it may be
+	 */
+	abstract public function __toString();
+
+	/**
+	 * Private method to set $this->root_el
+	 *
+	 * @param mixed $node Either node or node name to set as root element
+	 * @return self
+	 */
+	final protected function _setRoot($node = 'root')
+	{
+		if (is_string($node)) {
+			$this->body = $this->appendChild($this->createElement($node));
+		} elseif (is_object($node) and $node instanceof \DOMNode) {
+			$this->body = $this->appendChild($node);
+		} else {
+			trigger_error(
+				sprintf('$node must be either a DOMNode or string, %s given', gettype($node)),
+				E_USER_WARNING
+			);
+		}
+		return $this;
+	}
+}
---
+++ traits/magic/domelement.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * @author Chris Zuber <shgysk8zer0@gmail.com>
+ * @package shgysk8zer0\Core_API
+ * @version 1.0.0
+ * @copyright 2015, Chris Zuber
+ * @license http://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace shgysk8zer0\Core_API\Traits\Magic;
+/**
+ * Provides magic methods to \DOMElement... Make sure to extend it
+ * @see https://php.net/manual/en/class.domelement.php
+ */
+trait DOMElement
+{
+	/**
+	 * Magic alias of setAttribute
+	 *
+	 * @param string $name  The name of the attribute.
+	 * @param string $value The value of the attribute.
+	 * @return void
+	 */
+	final public function __set($name, $value = 'true')
+	{
+		if (substr($name, 0, 1) === '@') {
+			$this->setAttribute(substr($name, 1), $value);
+		} else {
+			if (is_string($value) or is_null($value)) {
+				$this->appendChild(new self($name, $value));
+			} elseif (is_object($value) and $value instanceof \DOMNode) {
+				$this->appendChild(new self($name))->appendChild($value);
+			} elseif (is_array($value)) {
+				$node = $this->appendChild(new self($name));
+				array_map(
+					function($key, $val) use (&$node)
+					{
+						$node->$key = $val;
+					},
+					array_keys($value),
+					array_values($value)
+				);
+			}
+		}
+	}
+
+	/**
+	 * Magic alias of getAttribute
+	 *
+	 * @param  string $name The name of the attribute.
+	 * @return string       The value of the attribute
+	 */
+	final public function __get($name)
+	{
+		return $this->getAttribute($name);
+	}
+
+	/**
+	 * Magic alias of hasAttribute
+	 *
+	 * @param  string  $name The name of the attribute
+	 * @return bool          TRUE on success or FALSE on failure.
+	 */
+	final public function __isset($name)
+	{
+		return $this->hasAttribute($name);
+	}
+
+	/**
+	 * Magic alias of removeAttribute
+	 *
+	 * @param string $name The name of the attribute
+	 * @return void
+	 */
+	final public function __unset($name)
+	{
+		$this->removeAttribute($name);
+	}
+
+	/**
+	 * Import text content when using class as a function
+	 *
+	 * @param  string $content XML or HTML
+	 * @return self
+	 * @example $this('<div>Content</div>')
+	 */
+	final public function __invoke($content = '')
+	{
+		$fragment = $this->ownerDocument->createDocumentFragment();
+		$fragment->appendXML($content);
+		$this->appendChild($fragment);
+		return $this;
+	}
+
+	/**
+	 * Return nodeValue of $this when used as a string
+	 *
+	 * @return string Node's text value as HTML or XML
+	 */
+	abstract public function __toString();
+}
