8ec36dd479eb2008d12bef4213f423c1776269ea
Chris Zuber <shgysk8zer0@gmail.com>
2015-04-29 10:32:20 -0700

    New HTML __toString trait
    Detects if class using it is `DOMDocument` or `DOMElement` and returns
    string accordingly

---
+++ traits/magic/html_string.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @author Chris Zuber <shgysk8zer0@gmail.com>
+ * @package shgysk8zer0\Core_API
+ * @version 1.0.0
+ * @copyright 2015, Chris Zuber
+ * @license http://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace shgysk8zer0\Core_API\Traits\Magic;
+
+/**
+ * Magic method to convert a DOMDocument or DOMElement to an HTML string
+ */
+trait HTML_String
+{
+	/**
+	* Return the document or element as an HTML string
+	*
+	* @param void
+	* @return string HTML content of document or element
+	*/
+	final public function __toString()
+	{
+		if ($this instanceof \DOMDocument) {
+			return $this->saveHTML();
+		} elseif ($this instanceof \DOMElement) {
+			return $this->ownerDocument->saveHTML($this);
+		}
+	}
+}
