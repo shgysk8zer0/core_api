6ffafb5de65a3e1760bb67bbd648d48315ab9632
Chris Zuber <shgysk8zer0@gmail.com>
2015-10-13 00:46:43 -0700

    Initial implementation of Unicode class for #27
    Contains class constants for miscellaneous Unicode characters

---
+++ abstracts/unicode.php
@@ -0,0 +1,284 @@
+<?php
+/**
+ * @author Chris Zuber <shgysk8zer0@gmail.com>
+ * @package shgysk8zer0\Core_API
+ * @subpackage Abstracts
+ * @version 1.0.0
+ * @copyright 2015, Chris Zuber
+ * @license http://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace shgysk8zer0\Core_API\Abstracts;
+
+/**
+ * Abstract class containing Unicode special characters
+ */
+abstract class Unicode
+{
+	const BLACK_SUN_WITH_RAYS = '&#9728;';
+	const CLOUD = '&#9729;';
+	const UMBRELLA = '&#9730;';
+	const SNOWMAN = '&#9731;';
+	const COMET = '&#9732;';
+	const BLACK_STAR = '&#9733;';
+	const WHITE_STAR = '&#9734;';
+	const LIGHTNING = '&#9735;';
+	const THUNDERSTORM = '&#9736;';
+	const SUN = '&#9737;';
+	const ASCENDING_NODE = '&#9738;';
+	const DESCENDING_NODE = '&#9739;';
+	const CONJUNCTION = '&#9740;';
+	const OPPOSITION = '&#9741;';
+	const BLACK_TELEPHONE = '&#9742;';
+	const WHITE_TELEPHONE = '&#9743;';
+	const BALLOT_BOX = '&#9744;';
+	const BALLOT_BOX_WITH_CHECK = '&#9745;';
+	const BALLOT_BOX_WITH_X = '&#9746;';
+	const SALTIRE = '&#9747;';
+	const UMBRELLA_WITH_RAIN_DROPS = '&#9748;';
+	const HOT_BEVERAGE = '&#9749;';
+	const WHITE_SHOGI_PIECE = '&#9750;';
+	const BLACK_SHOGI_PIECE = '&#9751;';
+	const SHAMROCK = '&#9752;';
+	const REVERSED_ROTATED_FLORAL_HEART_BULLET = '&#9753;';
+	const BLACK_LEFT_POINTING_INDEX = '&#9754;';
+	const BLACK_RIGHT_POINTING_INDEX = '&#9755;';
+	const WHITE_LEFT_POINTING_INDEX = '&#9756;';
+	const WHITE_UP_POINTING_INDEX = '&#9757;';
+	const WHITE_RIGHT_POINTING_INDEX = '&#9758;';
+	const WHITE_DOWN_POINTING_INDEX = '&#9759;';
+	const SKULL_AND_CROSSBONES = '&#9760;';
+	const CAUTION_SIGN = '&#9761;';
+	const RADIOACTIVE_SIGN = '&#9762;';
+	const BIOHAZARD_SIGN = '&#9763;';
+	const CADUCEUS = '&#9764;';
+	const ANKH = '&#9765;';
+	const ORTHODOX_CROSS = '&#9766;';
+	const CHI_RHO = '&#9767;';
+	const CROSS_OF_LORRAINE = '&#9768;';
+	const CROSS_OF_JERUSALEM = '&#9769;';
+	const STAR_AND_CRESCENT = '&#9770;';
+	const FARSI_SYMBOL = '&#9771;';
+	const ADI_SHAKTI = '&#9772;';
+	const HAMMER_AND_SICKLE = '&#9773;';
+	const PEACE_SYMBOL = '&#9774;';
+	const YIN_YANG = '&#9775;';
+	const TRIGRAM_FOR_HEAVEN = '&#9776;';
+	const TRIGRAM_FOR_LAKE = '&#9777;';
+	const TRIGRAM_FOR_FIRE = '&#9778;';
+	const TRIGRAM_FOR_THUNDER = '&#9779;';
+	const TRIGRAM_FOR_WIND = '&#9780;';
+	const TRIGRAM_FOR_WATER = '&#9781;';
+	const TRIGRAM_FOR_MOUNTAIN = '&#9782;';
+	const TRIGRAM_FOR_EARTH = '&#9783;';
+	const WHEEL_OF_DHARMA = '&#9784;';
+	const WHITE_FROWNING_FACE = '&#9785;';
+	const WHITE_SMILING_FACE_9786_ = '&#9786;';
+	const BLACK_SMILING_FACE = '&#9787;';
+	const WHITE_SUN_WITH_RAYS = '&#9788;';
+	const FIRST_QUARTER_MOON = '&#9789;';
+	const LAST_QUARTER_MOON = '&#9790;';
+	const MERCURY = '&#9791;';
+	const FEMALE_SIGN = '&#9792;';
+	const EARTH = '&#9793;';
+	const MALE_SIGN = '&#9794;';
+	const JUPITER = '&#9795;';
+	const SATURN = '&#9796;';
+	const URANUS = '&#9797;';
+	const NEPTUNE = '&#9798;';
+	const PLUTO = '&#9799;';
+	const ARIES = '&#9800;';
+	const TAURUS = '&#9801;';
+	const GEMINI = '&#9802;';
+	const CANCER = '&#9803;';
+	const LEO = '&#9804;';
+	const VIRGO = '&#9805;';
+	const LIBRA = '&#9806;';
+	const SCORPIUS = '&#9807;';
+	const SAGITTARIUS = '&#9808;';
+	const CAPRICORN = '&#9809;';
+	const AQUARIUS = '&#9810;';
+	const PISCES = '&#9811;';
+	const WHITE_CHESS_KING = '&#9812;';
+	const WHITE_CHESS_QUEEN = '&#9813;';
+	const WHITE_CHESS_ROOK = '&#9814;';
+	const WHITE_CHESS_BISHOP = '&#9815;';
+	const WHITE_CHESS_KNIGHT = '&#9816;';
+	const WHITE_CHESS_PAWN = '&#9817;';
+	const BLACK_CHESS_KING = '&#9818;';
+	const BLACK_CHESS_QUEEN = '&#9819;';
+	const BLACK_CHESS_ROOK = '&#9820;';
+	const BLACK_CHESS_BISHOP = '&#9821;';
+	const BLACK_CHESS_KNIGHT = '&#9822;';
+	const BLACK_CHESS_PAWN = '&#9823;';
+	const BLACK_SPADE_SUIT = '&#9824;';
+	const WHITE_HEART_SUIT = '&#9825;';
+	const WHITE_DIAMOND_SUIT = '&#9826;';
+	const BLACK_CLUB_SUIT = '&#9827;';
+	const WHITE_SPADE_SUIT = '&#9828;';
+	const BLACK_HEART_SUIT = '&#9829;';
+	const BLACK_DIAMOND_SUIT = '&#9830;';
+	const WHITE_CLUB_SUIT = '&#9831;';
+	const HOT_SPRINGS = '&#9832;';
+	const QUARTER_NOTE = '&#9833;';
+	const EIGHTH_NOTE = '&#9834;';
+	const BEAMED_EIGHTH_NOTES = '&#9835;';
+	const BEAMED_SIXTEENTH_NOTES = '&#9836;';
+	const MUSIC_FLAT_SIGN = '&#9837;';
+	const MUSIC_NATURAL_SIGN = '&#9838;';
+	const MUSIC_SHARP_SIGN = '&#9839;';
+	const WEST_SYRIAC_CROSS = '&#9840;';
+	const EAST_SYRIAC_CROSS = '&#9841;';
+	const UNIVERSAL_RECYCLING_SYMBOL = '&#9842;';
+	const RECYCLING_SYMBOL_FOR_TYPE_1_PLASTICS = '&#9843;';
+	const RECYCLING_SYMBOL_FOR_TYPE_2_PLASTICS = '&#9844;';
+	const RECYCLING_SYMBOL_FOR_TYPE_3_PLASTICS = '&#9845;';
+	const RECYCLING_SYMBOL_FOR_TYPE_4_PLASTICS = '&#9846;';
+	const RECYCLING_SYMBOL_FOR_TYPE_5_PLASTICS = '&#9847;';
+	const RECYCLING_SYMBOL_FOR_TYPE_6_PLASTICS = '&#9848;';
+	const RECYCLING_SYMBOL_FOR_TYPE_7_PLASTICS = '&#9849;';
+	const RECYCLING_SYMBOL_FOR_GENERIC_MATERIALS = '&#9850;';
+	const BLACK_UNIVERSAL_RECYCLING_SYMBOL = '&#9851;';
+	const RECYCLED_PAPER_SYMBOL = '&#9852;';
+	const PARTIALLY_RECYCLED_PAPER_SYMBOL = '&#9853;';
+	const PERMANENT_PAPER_SIGN = '&#9854;';
+	const WHEELCHAIR_SYMBOL = '&#9855;';
+	const DIE_FACE_1 = '&#9856;';
+	const DIE_FACE_2 = '&#9857;';
+	const DIE_FACE_3 = '&#9858;';
+	const DIE_FACE_4 = '&#9859;';
+	const DIE_FACE_5 = '&#9860;';
+	const DIE_FACE_6 = '&#9861;';
+	const WHITE_CIRCLE_WITH_DOT_RIGHT = '&#9862;';
+	const WHITE_CIRCLE_WITH_TWO_DOTS = '&#9863;';
+	const BLACK_CIRCLE_WITH_WHITE_DOT_RIGHT = '&#9864;';
+	const BLACK_CIRCLE_WITH_TWO_WHITE_DOTS = '&#9865;';
+	const MONOGRAM_FOR_YANG = '&#9866;';
+	const MONOGRAM_FOR_YIN = '&#9867;';
+	const DIGRAM_FOR_GREATER_YANG = '&#9868;';
+	const DIGRAM_FOR_LESSER_YIN = '&#9869;';
+	const DIGRAM_FOR_LESSER_YANG = '&#9870;';
+	const DIGRAM_FOR_GREATER_YIN = '&#9871;';
+	const WHITE_FLAG = '&#9872;';
+	const BLACK_FLAG = '&#9873;';
+	const HAMMER_AND_PICK = '&#9874;';
+	const ANCHOR = '&#9875;';
+	const CROSSED_SWORDS = '&#9876;';
+	const STAFF_OF_AESCULAPIUS = '&#9877;';
+	const SCALES = '&#9878;';
+	const ALEMBIC = '&#9879;';
+	const FLOWER = '&#9880;';
+	const GEAR = '&#9881;';
+	const STAFF_OF_HERMES = '&#9882;';
+	const ATOM_SYMBOL = '&#9883;';
+	const FLEUR_DE_LIS = '&#9884;';
+	const OUTLINED_WHITE_STAR = '&#9885;';
+	const THREE_LINES_CONVERGING_RIGHT = '&#9886;';
+	const THREE_LINES_CONVERGING_LEFT = '&#9887;';
+	const WARNING_SIGN = '&#9888;';
+	const HIGH_VOLTAGE_SIGN = '&#9889;';
+	const DOUBLED_FEMALE_SIGN = '&#9890;';
+	const DOUBLED_MALE_SIGN = '&#9891;';
+	const INTERLOCKED_FEMALE_AND_MALE_SIGN = '&#9892;';
+	const MALE_AND_FEMALE_SIGN = '&#9893;';
+	const MALE_WITH_STROKE_SIGN = '&#9894;';
+	const MALE_WITH_STROKE_AND_MALE_AND_FEMALE_SIGN = '&#9895;';
+	const VERTICAL_MALE_WITH_STROKE_SIGN = '&#9896;';
+	const HORIZONTAL_MALE_WITH_STROKE_SIGN = '&#9897;';
+	const MEDIUM_WHITE_CIRCLE = '&#9898;';
+	const MEDIUM_BLACK_CIRCLE = '&#9899;';
+	const MEDIUM_SMALL_WHITE_CIRCLE = '&#9900;';
+	const MARRIAGE_SYMBOL = '&#9901;';
+	const DIVORCE_SYMBOL = '&#9902;';
+	const UNMARRIED_PARTNERSHIP_SYMBOL = '&#9903;';
+	const COFFIN = '&#9904;';
+	const FUNERAL_URN = '&#9905;';
+	const NEUTER = '&#9906;';
+	const CERES = '&#9907;';
+	const PALLAS = '&#9908;';
+	const JUNO = '&#9909;';
+	const VESTA = '&#9910;';
+	const CHIRON = '&#9911;';
+	const BLACK_MOON_LILITH = '&#9912;';
+	const SEXTILE = '&#9913;';
+	const SEMISEXTILE = '&#9914;';
+	const QUINCUNX = '&#9915;';
+	const SESQUIQUADRATE = '&#9916;';
+	const SOCCER_BALL = '&#9917;';
+	const BASEBALL = '&#9918;';
+	const SQUARED_KEY = '&#9919;';
+	const WHITE_DRAUGHTS_MAN = '&#9920;';
+	const WHITE_DRAUGHTS_KING = '&#9921;';
+	const BLACK_DRAUGHTS_MAN = '&#9922;';
+	const BLACK_DRAUGHTS_KING = '&#9923;';
+	const SNOWMAN_WITHOUT_SNOW = '&#9924;';
+	const SUN_BEHIND_CLOUD = '&#9925;';
+	const RAIN = '&#9926;';
+	const BLACK_SNOWMAN = '&#9927;';
+	const THUNDER_CLOUD_AND_RAIN = '&#9928;';
+	const TURNED_WHITE_SHOGI_PIECE = '&#9929;';
+	const TURNED_BLACK_SHOGI_PIECE = '&#9930;';
+	const WHITE_DIAMOND_IN_SQUARE = '&#9931;';
+	const CROSSING_LANES = '&#9932;';
+	const DISABLED_CAR = '&#9933;';
+	const OPHIUCHUS = '&#9934;';
+	const PICK                                             = '&#9935;';
+	const CAR_SLIDING                                      = '&#9936;';
+	const HELMET_WITH_WHITE_CROSS                          = '&#9937;';
+	const CIRCLED_CROSSING_LANES                           = '&#9938;';
+	const CHAINS                                           = '&#9939;';
+	const NO_ENTRY                                         = '&#9940;';
+	const ALTERNATE_ONE_WAY_LEFT_WAY_TRAFFIC               = '&#9941;';
+	const BLACK_TWO_WAY_LEFT_WAY_TRAFFIC                   = '&#9942;';
+	const WHITE_TWO_WAY_LEFT_WAY_TRAFFIC                   = '&#9943;';
+	const BLACK_LEFT_LANE_MERGE                            = '&#9944;';
+	const WHITE_LEFT_LANE_MERGE                            = '&#9945;';
+	const DRIVE_SLOW_SIGN                                  = '&#9946;';
+	const HEAVY_WHITE_DOWN_POINTING_TRIANGLE               = '&#9947;';
+	const LEFT_CLOSED_ENTRY                                = '&#9948;';
+	const SQUARED_SALTIRE                                  = '&#9949;';
+	const FALLING_DIAGONAL_IN_WHITE_CIRCLE_IN_BLACK_SQUARE = '&#9950;';
+	const BLACK_TRUCK                                      = '&#9951;';
+	const RESTRICTED_LEFT_ENTRY_1                          = '&#9952;';
+	const RESTRICTED_LEFT_ENTRY_2                          = '&#9953;';
+	const ASTRONOMICAL_SYMBOL_FOR_URANUS                   = '&#9954;';
+	const HEAVY_CIRCLE_WITH_STROKE_AND_TWO_DOTS_ABOVE      = '&#9955;';
+	const PENTAGRAM                                        = '&#9956;';
+	const RIGHT_HANDED_INTERLACED_PENTAGRAM                = '&#9957;';
+	const LEFT_HANDED_INTERLACED_PENTAGRAM                 = '&#9958;';
+	const INVERTED_PENTAGRAM                               = '&#9959;';
+	const BLACK_CROSS_ON_SHIELD                            = '&#9960;';
+	const SHINTO_SHRINE                                    = '&#9961;';
+	const CHURCH                                           = '&#9962;';
+	const CASTLE                                           = '&#9964;';
+	const GEAR_WITHOUT_HUB                                 = '&#9965;';
+	const GEAR_WITH_HANDLES                                = '&#9966;';
+	const MAP_SYMBOL_FOR_LIGHTHOUSE                        = '&#9967;';
+	const MOUNTAIN                                         = '&#9968;';
+	const UMBRELLA_ON_GROUND                               = '&#9969;';
+	const FOUNTAIN                                         = '&#9970;';
+	const FLAG_IN_HOLE                                     = '&#9971;';
+	const FERRY                                            = '&#9972;';
+	const SAILBOAT                                         = '&#9973;';
+	const SQUARE_FOUR_CORNERS                              = '&#9974;';
+	const SKIER                                            = '&#9975;';
+	const ICE_SKATE                                        = '&#9976;';
+	const PERSON_WITH_BALL                                 = '&#9977;';
+	const TENT                                             = '&#9978;';
+	const JAPANESE_BANK_SYMBOL                             = '&#9979;';
+	const HEADSTONE_GRAVEYARD_SYMBOL                       = '&#9980;';
+	const FUEL_PUMP                                        = '&#9981;';
+	const CUP_ON_BLACK_SQUARE                              = '&#9982;';
+	const WHITE_FLAG_WITH_HORIZONTAL_MIDDLE_BLACK_STRIPE   = '&#9983;';
+}
